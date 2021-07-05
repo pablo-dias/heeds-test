58e299ff789b1e049454605bb575af4499cbe736
Pablo Dias <pablo.dias.f@icloud.com>
2021-07-05 15:36:24 -0300

    Initial commit

---
+++ src/components/Task.js
@@ -0,0 +1,33 @@
+
+import { FaTimes, FaCheckCircle, FaPen } from 'react-icons/fa'
+import { useState } from 'react'
+
+const Task = ( { task, onDelete, onToggle, edit} ) => {
+    const [showDiv,setShowDiv] = useState(false);
+    const [text,setText] = useState('');
+    // const [date,setDate] = useState('');
+  
+    const show=()=>{
+        setShowDiv(!showDiv);
+        setText(task.text);
+        // setDate(task.date);
+    }
+    const save=(id)=>{
+        setShowDiv(!showDiv);
+        edit(id,text);
+    }
+
+    return (
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}  >
+            <h3>{showDiv?<input type='text' className='text-line' value={text} onChange={(e)=>setText(e.target.value)} />: <p>{task.text}</p> } <FaTimes style={{ color: 'red', cursor: 'pointer'}} onClick={() => onDelete(task.id)} /> </h3>
+            <p>
+                {/* {showDiv?<input type='text' className='text-line' value={date} 
+                onChange={(e)=>setDate(e.target.value)} />: <label>{task.date}</label> } */}
+                {showDiv?<FaCheckCircle style={{color:'green',cursor:'pointer'}} 
+                onClick={()=>save(task.id)}/>:<FaPen style={{color:'green',cursor:'pointer'}} onClick={show} />}
+            </p>
+        </div>
+    )
+}
+
+export default Task
